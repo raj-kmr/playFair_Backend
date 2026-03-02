4482200a8f267e7fd00fe007605b979c14f574a9
Raj <raj3373y@gmail.com>
2026-03-02 15:07:48 +0530

    Mount the uploadRoutes

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const port = process.env.PORT;
+const uploadRoutes = require("./routes/uploads")
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+    console.log("INCOMING: ", req.method, req.url)
+    next();
+})
+
 app.use("/igdb", require("./routes/igdb.router"));
+
+// Serve uploaded files publicaly, Replace with S3 later
+app.use("/uploads", express.static("uploads")) // serve uploaded files
+app.use("/uploads", uploadRoutes) // POST /uploads/images
 
 
 app.get("/test", (req, res) => {
