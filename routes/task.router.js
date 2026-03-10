f37ef94a76f59ad4e5fac18981e31079bb3af4ee
Raj <raj3373y@gmail.com>
2026-03-10 19:17:15 +0530

    work: final routes for crating task and changing their status

---
+++ routes/task.router.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const router = express.Router();
+
+const {
+    createTask,
+    getTasks,
+    updateTaskDailyStatus,
+    getTaskDailyStatus
+} = require("../controllers/task.controller")
+const authMiddlware = require("../middleware/auth.middleware");
+
+router.post("/tasks", authMiddlware, createTask)
+router.get("/tasks", authMiddlware, getTasks)
+router.patch("/tasks/:id/daily-status", authMiddlware, updateTaskDailyStatus)
+router.get("/tasks/daily-status", authMiddlware, getTaskDailyStatus)
+
+module.exports = router;
