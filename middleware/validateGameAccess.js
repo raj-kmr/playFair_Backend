b012f45a751263da228e337ac100f5393b599f85
Raj <raj3373y@gmail.com>
2026-03-20 11:50:46 +0530

    work: middleware to check if session creation is allowed or not

---
+++ middleware/validateGameAccess.js
@@ -0,0 +1,28 @@
+const getAvailableMinutes = require("../services/unlock.service")
+
+async function validateGameAccess(req, res, next) {
+    try {
+        const userId = req.user.id;
+
+        const result = await getAvailableMinutes(userId);
+
+        const availableMinutes = result.availableMinutes;
+
+        if (availableMinutes <= 0) {
+            return res.status(403).json({
+                error: "No gaming time available. Complete tasks first"
+            })
+        }
+        next();
+    } catch (err) {
+        console.error("Unlock gaming middleware error: ", err);
+        
+        res.status(403).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+module.exports = {
+    validateGameAccess
+}
