73f76d2672032b496ddc1ab4e8f8dcabe7a7f336
Raj <raj3373y@gmail.com>
2026-03-22 13:54:12 +0530

    work: Business logic for analytics in app

---
+++ services/analytics.service.js
@@ -0,0 +1,104 @@
+const pool = require("../config/db")
+
+async function getTotalPlaytime (userId) {
+    const query = `
+        SELECT
+            COALESCE(SUM(duration_minutes), 0) AS total_minutes
+        FROM game_sessions
+        WHERE users_id = $1
+        AND ended_at IS NOT NULL
+    `
+
+    const { rows } = await pool.query(query, [userId])
+
+    return rows[0];
+}
+
+// get playtime for each day (7 days)
+async function getWeeklyPlaytime (userId) {
+    const query = `
+        SELECT
+            DATE(started_at) AS day,
+            COALESCE(SUM(duration_minutes), 0) AS minutes
+        FROM game_sessions
+        WHERE users_id = $1
+            AND started_at >= NOW() - INTERVAL '7 days'
+            AND ended_at IS NOT NULL
+        GROUP BY day
+        ORDER BY day ASC
+    `
+
+    const {rows} = await pool.query(query, [userId])
+
+    return rows;
+}
+
+// Get playtime group by week (last 1 month)
+async function getMonthlyPlaytime(userId) {
+    const query = `
+        SELECT
+            DATE_TRUNC("week", started_at) AS week,
+            COALESCE(SUM(duration_minutes), 0) AS minutes
+        FROM game_sessions
+        WHERE users_id = $1
+            AND started_at >= NOW() - INTERVAL "1 month"
+            AND ended_at IS NOT NULL
+        GROUP BY week
+        ORDER BY week ASC
+    `
+
+    const { rows } = await pool.query(query, [userId])
+
+    return rows;
+}
+
+// Session Analytics
+// Get session statistics
+async function getSessionStats () {
+    const query = `
+        SELECT 
+            COUNT(*) AS total_sessions,
+            COALESCE(AVG(duration_minutes), 0) AS avg_session_minutes,
+            COALESCE(MAX(duration_minutes), 0) AS max_session_minutes
+        FROM game_sessions
+        WHERE users_id = $1
+            AND ended_at IS NOT NULL
+    `
+
+    const { rows }  = await pool.query(query, [userId])
+
+    return rows[0];
+}
+
+// Task analytics
+// Get task completion rate
+async function getTaskCompletionRate() {
+    const query = `
+        SELECT
+            COUNT(*) FILTER(WHERE is_completed = true) AS completed,
+            COUNT(*) AS total
+        FROM task_daily_status
+        WHERE user_id = $1
+            AND date >= NOW() - INTERVAL "7 days"
+    `
+
+    const { rows } = await pool.query(query, [userId])
+
+    const completed = parseInt(rows[0].completed, 10)
+    const total = parseInt(rows[0].total, 10)
+    const percentage = total === 0 ? 0 : (completed / total) * 100;
+
+    return {
+        completed,
+        total,
+        percentage
+    }
+}
+
+module.exports = {
+    getTotalPlaytime,
+    getWeeklyPlaytime,
+    getMonthlyPlaytime,
+    getSessionStats,
+    getTaskCompletionRate
+}
